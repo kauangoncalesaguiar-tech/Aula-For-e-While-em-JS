dc51cde52999a6aee2fc2311b68d765b115cab12
kauangoncalesaguiar-tech <kauan.goncales.aguiar@escola.pr.gov.br>
2026-06-19 10:57:48 -0300

    quase perdido

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-
-const alunos = ['Kauan', 'Jão', 'Roberto', 'Gabriela', 'Jurunberto', 'Pedro', 'Lucas', 'Alucard', 'Ryan', 'Maria'];
+//Notas
 const notas = [7, 8, 9, 10, 2, 6, 9, 3, 8, 9];
+//Media da sala inteira
+let soma = 0;
+for (let i = 0; i < notas.length; i++) {
+    soma += notas[i];};
 
+let media = soma / notas.length;
+//console.log(media);
 
+//Maior numero
+let MaiorNumero = Math.max(...notas);
+//console.log(MaiorNumero);
 
-console.log(counter);
+//Menor numero
+let MenorNumero = Math.min(...notas);
+//console.log(MenorNumero)
 
+let notaSete = notas.filter(notas => notas >= 7);
+//console.log(notaSete);
+ 

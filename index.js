85c81dd69bcef4c3330783241dc6ac0baacdde39
kauangoncalesaguiar-tech <kauan.goncales.aguiar@escola.pr.gov.br>
2026-06-19 09:08:53 -0300

    socorro

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+const alunos = ['Kauan', 'Jão', 'Roberto', 'Gabriela', 'Jurunberto', 'Pedro', 'Lucas', 'Alucard', 'Ryan', 'Maria'];
+const notas = [7, 8, 9, 10, 2, 6, 9, 3, 8, 9];
+
+
+
+console.log(counter);
+
